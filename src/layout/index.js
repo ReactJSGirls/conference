4f7df1b1c83f83345c0ba0fa8a421e696d4cf8c5
Sara Vieira <hey@iamsaravieira.com>
2018-12-05 15:30:21 +0100

    add seo things

--- src/layout/index.js
+++ src/layout/index.js
@@ -27,6 +27,32 @@ const Template = ({ children }) => {
             <html lang="en" />
             <title>{edges[0].node.seoTitle}</title>
             <meta name="description" content={edges[0].node.seoDescription} />
+            <meta charset="utf-8" />
+            <meta
+              name="image"
+              content="https://rawcdn.githack.com/ReactJSGirls/conference/0668dada75f299cb2cabdd142a6f4277eb378372/src/assets/favicon.png"
+            />
+            <meta itemProp="name" content={edges[0].node.seoTitle} />
+            <meta
+              itemProp="description"
+              content={edges[0].node.seoDescription}
+            />
+            <meta
+              itemProp="image"
+              content="https://rawcdn.githack.com/ReactJSGirls/conference/0668dada75f299cb2cabdd142a6f4277eb378372/src/assets/favicon.png"
+            />
+            <meta name="og:title" content={edges[0].node.seoTitle} />
+            <meta
+              name="og:description"
+              content={edges[0].node.seoDescription}
+            />
+            <meta
+              name="og:image"
+              content="https://rawcdn.githack.com/ReactJSGirls/conference/0668dada75f299cb2cabdd142a6f4277eb378372/src/assets/favicon.png"
+            />
+            <meta name="og:url" content="https://reactjsgirls.com" />
+            <meta name="og:site_name" content={edges[0].node.seoTitle} />
+            <meta name="og:type" content="website" />
           </Helmet>
           <Navigation />
           <Global styles={styles} />
