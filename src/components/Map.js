eb9d375bbdc4ec816aa6d601546db68a69625d60
Sara Vieira <hey@iamsaravieira.com>
2019-01-16 14:47:20 +0100

    move mapstyles

--- src/components/Map.js
+++ src/components/Map.js
@@ -6,93 +6,13 @@ import {
   GoogleMap,
   Marker
 } from 'react-google-maps'
+import styles from '../utils/mapStyles'
 
-var styles = [
-  {
-    featureType: 'all',
-    elementType: 'all',
-    stylers: [
-      {
-        visibility: 'on'
-      }
-    ]
-  },
-  {
-    featureType: 'administrative',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#444444'
-      }
-    ]
-  },
-  {
-    featureType: 'landscape',
-    elementType: 'all',
-    stylers: [
-      {
-        color: '#f2f2f2'
-      }
-    ]
-  },
-  {
-    featureType: 'poi',
-    elementType: 'all',
-    stylers: [
-      {
-        visibility: 'off'
-      }
-    ]
-  },
-  {
-    featureType: 'road',
-    elementType: 'all',
-    stylers: [
-      {
-        saturation: -100
-      },
-      {
-        lightness: 45
-      }
-    ]
-  },
-  {
-    featureType: 'road.highway',
-    elementType: 'all',
-    stylers: [
-      {
-        visibility: 'simplified'
-      }
-    ]
-  },
-  {
-    featureType: 'road.arterial',
-    elementType: 'labels.icon',
-    stylers: [
-      {
-        visibility: 'off'
-      }
-    ]
-  },
-
-  {
-    featureType: 'water',
-    elementType: 'all',
-    stylers: [
-      {
-        color: '#e8cfc7'
-      },
-      {
-        visibility: 'on'
-      }
-    ]
-  }
-]
 const MapComponent = compose(
   withProps({
     googleMapURL:
       'https://maps.googleapis.com/maps/api/js?key=AIzaSyDMpDkPdvwEG9mxQ3sA6vaKrq64V7trj_4',
-    loadingElement: <div style={{ height: `100%` }} />,
+    loadingElement: <div style={{ height: 400 }} />,
     containerElement: (
       <div
         style={{
@@ -103,7 +23,7 @@ const MapComponent = compose(
         }}
       />
     ),
-    mapElement: <div style={{ height: `100%` }} />
+    mapElement: <div style={{ height: 400 }} />
   }),
   withScriptjs,
   withGoogleMap
