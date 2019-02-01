2b4144ff9f4c5e44825ffe445ef38891a0e2a4ad
Sara Vieira <hey@iamsaravieira.com>
2019-02-01 19:02:23 +0100

    remove diveir

--- src/components/Divider.js
+++ src/components/Divider.js
@@ -2,7 +2,4 @@ import styled from 'styled-components'
 
 export default styled.div`
   margin-top: 430px;
-  height: 320px;
-  background-size: cover;
-  background-image: url("data:image/svg+xml,%3C%3Fxml version='1.0' encoding='UTF-8'%3F%3E%3Csvg width='1440px' height='448px' viewBox='0 0 1440 448' version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'%3E%3C!-- Generator: Sketch 52.6 (67491) - http://www.bohemiancoding.com/sketch --%3E%3Ctitle%3EGroup 10%3C/title%3E%3Cdesc%3ECreated with Sketch.%3C/desc%3E%3Cg id='Page-1' stroke='none' stroke-width='1' fill='none' fill-rule='evenodd'%3E%3Cg id='reactjs-girls-conf' transform='translate(0.000000, -872.000000)' fill='%23F3F4F9' fill-rule='nonzero'%3E%3Cg id='Group-10' transform='translate(-257.000000, 872.000000)'%3E%3Cpath d='M0,427.22333 L1750,433 C1750,287.544298 1750,205.508018 1750,186.891162 C1750,61.6834822 1516.43573,-43.9291362 1266.86828,74.3338097 C1110.03904,148.650745 869.787529,200.941613 661.498184,74.3338097 C471.532602,-41.1359717 0,-28.1123139 0,156.0485 C0,278.822376 0,369.213986 0,427.22333 Z' id='Oval' fill-opacity='0.4'%3E%3C/path%3E%3Cpath d='M230,443.403684 L1702,448 C1701.56743,236.388664 1701.56617,122.148308 1701.99623,105.278932 C1703.83776,33.0436525 1030.179,166.009912 946.174103,172.715126 C528.428519,206.059293 230,-71.9997525 230,120.414461 C230,248.690604 230,356.353678 230,443.403684 Z' id='Oval'%3E%3C/path%3E%3C/g%3E%3C/g%3E%3C/g%3E%3C/svg%3E%0A");
 `
