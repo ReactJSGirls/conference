4a54ddf98d3bd2ddd538ad9d34436ca0876f1ac6
Jennifer Spencer <jennifer.spencer@yld.io>
2019-04-05 16:31:46 +0100

    adjust spacing of sponsors & community section

--- src/components/Community.js
+++ src/components/Community.js
@@ -21,6 +21,11 @@ const Wrapper = styled.section`
   padding-bottom: 100px;
   padding-top: 0;
 `
+
+const SubSection = styled.section`
+  padding-bottom: 40px;
+`
+
 const About = ({
   intro: { intro },
   partners,
@@ -31,142 +36,147 @@ const About = ({
   <Wrapper>
     <Page fancy>
       <PageTitle>Sponsors & Community</PageTitle>
-      <PageTitle
-        css={`
-          font-size: 28px;
-          margin-top: 40px;
-          padding-bottom: 30px;
-        `}
-      >
-        Sponsors
-      </PageTitle>
-      <ReactMarkdown source={sponsorsThankYou} />
-      <h4
-        css={`
-          margin-top: 20px;
-        `}
-      >
-        Organiser
-      </h4>
-      <a rel="noopener noreferrer" href="https://yld.io" target="_blank">
-        {' '}
-        <img
-          width="130"
-          src="https://uploads-ssl.webflow.com/5b3dfe51f5523c976ad4b12b/5b436659d4f8bb51f436538a_*YLD%20logo%20-%20main-p-500.jpeg"
-        />
-      </a>
 
-      {partners.filter(s => s.sponsorType === 'Diversity').length ? (
-        <Fragment>
-          <h4
-            css={`
-              margin-top: 20px;
-            `}
-          >
-            Diversity Sponsors
-          </h4>
+      <SubSection>
+        <PageTitle
+          css={`
+            font-size: 28px;
+            padding-bottom: 30px;
+          `}
+        >
+          Sponsors
+        </PageTitle>
+        <ReactMarkdown source={sponsorsThankYou} />
+        <h4
+          css={`
+            margin-top: 40px;
+          `}
+        >
+          Organiser
+        </h4>
+        <a rel="noopener noreferrer" href="https://yld.io" target="_blank">
+          {' '}
+          <img
+            width="130"
+            src="https://uploads-ssl.webflow.com/5b3dfe51f5523c976ad4b12b/5b436659d4f8bb51f436538a_*YLD%20logo%20-%20main-p-500.jpeg"
+          />
+        </a>
 
-          <Communities>
-            {partners
-              .filter(s => s.sponsorType === 'Diversity')
-              .map(partner => (
-                <a
-                  key={partner.id}
-                  rel="noopener noreferrer"
-                  href={partner.link}
-                  target="_blank"
-                >
-                  {' '}
-                  <img height="95" src={partner.logo.file.url} />
-                </a>
-              ))}
-          </Communities>
-        </Fragment>
-      ) : null}
-
-      {partners.filter(s => s.sponsorType === 'Sponsor').length ? (
-        <Fragment>
-          <h4
-            css={`
-              margin-top: 20px;
-            `}
-          >
-            Community Sponsors
-          </h4>
-
-          <Communities styles={{ width: '50px'}}>
-            {partners
-              .filter(s => s.sponsorType === 'Sponsor')
-              .map(partner => (
-                <a
-                  key={partner.id}
-                  rel="noopener noreferrer"
-                  href={partner.link}
-                  target="_blank"
-                >
-                  {' '}
-                  <img width="200" src={partner.logo.file.url}/>
-                </a>
-              ))}
-          </Communities>
-        </Fragment>
-      ) : null}
-
-      <ReactMarkdown
-        css={`
-          margin: 40px 0;
-        `}
-        source={joinConversation}
-      />
-      <Button
-        rel="noopener noreferrer"
-        href="https://yld.typeform.com/to/xz4xYb"
-        target="_blank"
-        alternative
-        css={`
-          margin-top: 40px;
-        `}
-      >
-        <span>Sponsor the event</span>
-      </Button>
-
-      <p
-        css={`
-          margin-top: 40px;
-        `}
-      >
-        <ReactMarkdown source={diversityText} />
-      </p>
-      <PageTitle
-        css={`
-          font-size: 28px;
-          margin-top: 40px;
-          padding-bottom: 30px;
-        `}
-      >
-        Community
-      </PageTitle>
-      <ReactMarkdown
-        css={`
-          margin-bottom: 40px;
-        `}
-        source={intro}
-      />
-      <Communities>
-        {partners
-          .filter(s => !s.sponsorType)
-          .map(partner => (
-            <a
-              key={partner.id}
-              rel="noopener noreferrer"
-              href={partner.link}
-              target="_blank"
+        {partners.filter(s => s.sponsorType === 'Diversity').length ? (
+          <Fragment>
+            <h4
+              css={`
+                margin-top: 40px;
+              `}
             >
-              {' '}
-              <img height="95" src={partner.logo.file.url} />
-            </a>
-          ))}
-      </Communities>
+              Diversity Sponsors
+            </h4>
+
+            <Communities>
+              {partners
+                .filter(s => s.sponsorType === 'Diversity')
+                .map(partner => (
+                  <a
+                    key={partner.id}
+                    rel="noopener noreferrer"
+                    href={partner.link}
+                    target="_blank"
+                  >
+                    {' '}
+                    <img height="95" src={partner.logo.file.url} />
+                  </a>
+                ))}
+            </Communities>
+          </Fragment>
+        ) : null}
+
+        {partners.filter(s => s.sponsorType === 'Sponsor').length ? (
+          <Fragment>
+            <h4
+              css={`
+                margin-top: 40px;
+              `}
+            >
+              Community Sponsors
+            </h4>
+
+            <Communities styles={{ width: '50px'}}>
+              {partners
+                .filter(s => s.sponsorType === 'Sponsor')
+                .map(partner => (
+                  <a
+                    key={partner.id}
+                    rel="noopener noreferrer"
+                    href={partner.link}
+                    target="_blank"
+                  >
+                    {' '}
+                    <img width="200" src={partner.logo.file.url}/>
+                  </a>
+                ))}
+            </Communities>
+          </Fragment>
+        ) : null}
+
+        <ReactMarkdown
+          css={`
+            margin: 40px 0;
+          `}
+          source={joinConversation}
+        />
+        <Button
+          rel="noopener noreferrer"
+          href="https://yld.typeform.com/to/xz4xYb"
+          target="_blank"
+          alternative
+          css={`
+            margin-top: 40px;
+          `}
+        >
+          <span>Sponsor the event</span>
+        </Button>
+
+        <p
+          css={`
+            margin-top: 40px;
+          `}
+        >
+          <ReactMarkdown source={diversityText} />
+        </p>
+      </SubSection>
+
+      <SubSection>
+        <PageTitle
+          css={`
+            font-size: 28px;
+            margin-top: 40px;
+            padding-bottom: 30px;
+          `}
+        >
+          Community
+        </PageTitle>
+        <ReactMarkdown
+          css={`
+            margin-bottom: 40px;
+          `}
+          source={intro}
+        />
+        <Communities>
+          {partners
+            .filter(s => !s.sponsorType)
+            .map(partner => (
+              <a
+                key={partner.id}
+                rel="noopener noreferrer"
+                href={partner.link}
+                target="_blank"
+              >
+                {' '}
+                <img height="95" src={partner.logo.file.url} />
+              </a>
+            ))}
+        </Communities>
+      </SubSection>
     </Page>
   </Wrapper>
 )
